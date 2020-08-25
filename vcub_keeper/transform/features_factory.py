d8a1c6d3ce78fc957f3149c44df35916ea957cc2
Gillesa <arm.gilles@gmail.com>
2020-08-25 18:12:02 +0200

    Création de la features 'transactions_ajout' + analyse. Fix #4

--- vcub_keeper/transform/features_factory.py
+++ vcub_keeper/transform/features_factory.py
@@ -1,6 +1,5 @@
 import pandas as pd 
 
-import pandas as pd 
 
 def get_transactions_depot(data):
     """
@@ -34,5 +33,43 @@ def get_transactions_depot(data):
     
     # Drop non usefull column
     data.drop('available_stand_shift', axis=1, inplace=True)
+    
+    return data
+
+import pandas as pd 
+
+
+def get_transactions_ajout(data):
+    """
+    Calcul le nombre d'ajout de vélo qu'il y a eu pour une même station entre 2 points de données
+    
+    Parameters
+    ----------
+    data : DataFrame
+        Activité des stations Vcub
+    
+    Returns
+    -------
+    data : DataFrame
+        Ajout de colonne 'transactions_ajout'
+        
+    Examples
+    --------
+    
+    activite = get_transactions_ajout(activite)
+    """
+    
+    data['available_bike_shift'] = \
+    data.groupby('station_id')['available_bike'].shift(1)
+
+    data['available_bike_shift'] = data['available_bike_shift'].fillna(data['available_bike'])
+
+    data['transactions_ajout'] = data['available_bike'] - data['available_bike_shift']
+
+    data.loc[data['transactions_ajout'] < 0,
+                 'transactions_ajout'] = 0
+    
+    # Drop non usefull column
+    data.drop('available_bike_shift', axis=1, inplace=True)
     
     return data
