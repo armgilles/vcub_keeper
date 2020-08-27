31129365db74e3230912af13c95d283a47bd2b3a
Gillesa <arm.gilles@gmail.com>
2020-08-27 11:20:55 +0200

    Fichier configuration pour faciliter l'organisation des data du projet. Fix #13

---
+++ vcub_keeper/config.py
@@ -0,0 +1,27 @@
+import os
+import numpy as np
+
+# Paths
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+# If package is install with pip, ROOT_DIR is bad
+if "site-packages" in ROOT_DIR:  # isntall via pip
+    ROOT_DIR = os.path.realpath("")
+
+ROOT_DATA_RAW = ROOT_DIR + '/data/raw/'
+ROOT_DATA_CLEAN = ROOT_DIR + '/data/clean/'
+ROOT_DATA_REF = ROOT_DIR + '/data/ref/'
+
+# ROOT_DATA_RAW
+if not os.path.exists(ROOT_DATA_RAW):
+    os.mkdir(ROOT_DATA_RAW)
+    print('Create '+ROOT_DATA_RAW)
+
+# ROOT_DATA_CLEAN
+if not os.path.exists(ROOT_DATA_CLEAN):
+    os.mkdir(ROOT_DATA_CLEAN)
+    print('Create '+ROOT_DATA_CLEAN)
+
+# ROOT_DATA_REF
+if not os.path.exists(ROOT_DATA_REF):
+    os.mkdir(ROOT_DATA_REF)
+    print('Create '+ROOT_DATA_REF)
