221c0fa5b6ac53e8f6f620a0b0ff880fcdc95056
Gillesa <arm.gilles@gmail.com>
2020-08-27 17:07:20 +0200

    Change source file. Update function to read Ref stations. Fix #16

--- vcub_keeper/reader/reader.py
+++ vcub_keeper/reader/reader.py
@@ -1,17 +1,22 @@
 import pandas as pd
 
+from vcub_keeper.config import ROOT_DATA_RAW, ROOT_DATA_REF
 
-def read_stations_attributes(file_path = "../../data/bordeaux-stations.csv"):
+
+def read_stations_attributes(file_name="tb_stvel_p.csv"):
     """
-    Lecture du fichier sur les attributs des Vcub à Bordeaux
+    Lecture du fichier sur les attributs des Vcub à Bordeaux.
+    Ce fichier est situé dans ROOT_DATA_REF
     Modification par rapport au fichier original : 
         - Changement de nom des colonnnes :
-            - ident -> station_id
+            - NBSUPPOR -> total_stand
+            - NUMSTAT -> station_id
+        - Création des features lon & lat features from 'Geo Point'
     
     Parameters
     ----------
-    file_path : str
-        Chemin d'accès au fichiers source
+    file_name : str
+        Nom du fichier
     
     Returns
     -------
@@ -23,15 +28,22 @@ def read_stations_attributes(file_path = "../../data/bordeaux-stations.csv"):
     stations = read_stations_attributes()
     """
     
-    column_dtypes = {'ident': 'uint8',
-                     'nom': 'string',
-                    }
-
-
-    stations = pd.read_csv(file_path, dtype = column_dtypes)
+    column_dtypes = {'NUMSTAT': 'uint8'}
+    usecols = ['Geo Point', 'Geo Shape', 'COMMUNE', 'NBSUPPOR',
+              'NOM', 'TYPEA', 'ADRESSE', 'TARIF', 'NUMSTAT']
     
-    # Renaming
-    stations.rename(columns={'ident':'station_id'}, inplace=True)
+    stations = pd.read_csv(ROOT_DATA_REF+file_name, sep=';',
+                           dtype=column_dtypes, usecols=usecols)
+
+    # Naming
+    stations.rename(columns={'NBSUPPOR': 'total_stand'}, inplace=True)
+    stations.rename(columns={'NUMSTAT': 'station_id'}, inplace=True)
+
+    # Create lon / lat
+    stations['lat'] = stations['Geo Point'].apply(lambda x : x.split(',')[0])
+    stations['lat'] = stations['lat'].astype(float)
+    stations['lon'] = stations['Geo Point'].apply(lambda x : x.split(',')[1])
+    stations['lon'] = stations['lon'].astype(float)
     
     return stations
 
