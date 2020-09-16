15312e283ae91a80a71503ccc8f345040cc039c5
Gillesa <arm.gilles@gmail.com>
2020-09-16 22:02:59 +0200

    Création d'un fichier meteo et industrialisation. Fix #24

--- vcub_keeper/create/creator.py
+++ vcub_keeper/create/creator.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import glob as glob
+from dotenv import load_dotenv
+load_dotenv()
 
 from vcub_keeper.config import ROOT_DATA_RAW, ROOT_DATA_CLEAN
 from vcub_keeper.transform.features_factory import (get_transactions_in, get_transactions_out,
@@ -92,3 +94,90 @@ def create_activity_time_series():
     
     # Export
     activite_full_resample.to_hdf(ROOT_DATA_CLEAN + 'time_serie_activity.h5', key='ts_activity')
+
+
+def create_meteo(min_date_history="2018-12-01", max_date_history='2020-09-18'):
+    """
+    Multiple call API afin de créer un fichier et d'exporte celui-ci
+    dans ROOT_DATA_REF/meteo.csv
+    
+    Parameters
+    ----------
+    min_date_history : str
+        date du début de l'historique (format 'yyyy-mm-dd')
+    max_date_history : str
+        date de fin de l'historique (format 'yyyy-mm-dd')
+    
+    Returns
+    -------
+    None
+        
+    Examples
+    --------
+    
+    create_meteo(min_date_history="2018-12-01", max_date_history='2021-03-18')
+    """
+    api = Api(API_METEO)
+    api.set_granularity('daily')
+    
+    # Init DataFrame
+    meteo_full = pd.DataFrame()
+
+    # Calls API
+    date_list = pd.date_range(start=min_date_history, end=max_date_history)
+    for date in date_list:
+        # Date processing
+        date_minus_one_day = date - datetime.timedelta(days=1)
+        date_str = date.strftime(format='%Y-%m-%d')
+        date_minus_one_day_str = date_minus_one_day.strftime(format='%Y-%m-%d')
+        print(date_minus_one_day_str + " " + date_str)
+
+        # Call API
+        try:
+            history = api.get_history(city="Bordeaux", country="FR",
+                                      start_date=date_minus_one_day_str,
+                                      end_date=date_str)
+            meteo_day = pd.DataFrame(history.get_series(['max_temp', 'min_temp', 
+                                                         'precip','temp','rh', 'pres']))
+            meteo_full = pd.concat([meteo_full, meteo_day])
+        except requests.HTTPError as exception:
+            print(exception)
+
+    # Naming DataFrame
+    
+    # Accumulated precipitation (default mm)
+    meteo_full.rename(columns = {'precip':'precipitation'}, inplace = True)
+    # Average temperature
+    meteo_full.rename(columns = {'temp':'mean_teamp'}, inplace = True)
+    # Average relative humidity (%)
+    meteo_full.rename(columns = {'rh':'humidity_mean'}, inplace = True)
+    # Average pressure (mb)
+    meteo_full.rename(columns = {'pres':'pressure_mean'}, inplace = True)
+    # date
+    meteo_full.rename(columns = {'datetime':'date'}, inplace = True)
+
+    meteo_full = meteo_full[['date', 'min_temp', 'mean_teamp', 'max_temp',
+                             'pressure_mean', 'humidity_mean',
+                             'precipitation']]
+    
+    # Check
+    min_date = meteo_full.date.min()
+    max_date = meteo_full.date.max()
+    date_ref = pd.date_range(start=min_date, end=max_date, freq='d')
+
+    # Si le référenciel n'a pas toutes les dates dans Timestamp
+    assert date_ref.isin(meteo_full['date']).all() == True
+
+    # Si il n'y a pas de différence symetrique entre les 2 séries de dates
+    assert len(date_ref.symmetric_difference(meteo_full['date'])) == 0 
+
+    # Si il y a des doublons
+    assert meteo_full['date'].is_unique == True
+
+    # Si les date augmentent
+    assert meteo_full['date'].is_monotonic_increasing == True
+    
+    # export
+    max_date_full = meteo_full.date.max().strftime('%Y-%m-%d')
+    meteo_full.to_csv(ROOT_DATA_REF+'meteo/meteo_'+max_date_full+'.csv',
+                     index=False)
