823ac96450da75df2b38badc817bca664916884d
Gillesa <arm.gilles@gmail.com>
2020-10-06 16:30:33 +0200

    Clustering with IF to detect anomalies by station. Fix #23

--- vcub_keeper/config.py
+++ vcub_keeper/config.py
@@ -10,6 +10,7 @@ if "site-packages" in ROOT_DIR:  # isntall via pip
 ROOT_DATA_RAW = ROOT_DIR + '/data/raw/'
 ROOT_DATA_CLEAN = ROOT_DIR + '/data/clean/'
 ROOT_DATA_REF = ROOT_DIR + '/data/ref/'
+ROOT_MODEL = ROOT_DIR + '/model/'
 
 # ROOT_DATA_RAW
 if not os.path.exists(ROOT_DATA_RAW):
@@ -26,6 +27,11 @@ if not os.path.exists(ROOT_DATA_REF):
     os.mkdir(ROOT_DATA_REF)
     print('Create '+ROOT_DATA_REF)
 
+# ROOT_MODEL
+if not os.path.exists(ROOT_MODEL):
+    os.mkdir(ROOT_MODEL)
+    print('Create '+ROOT_MODEL)
+
 SEED = 2020
 
 # Key api meteo
@@ -34,3 +40,18 @@ API_METEO = os.getenv("API_METEO")
 # Station Vcub ID non user
 # https://github.com/armgilles/vcub_keeper/issues/29#issuecomment-703246491
 NON_USE_STATION_ID = [244, 249, 250]
+
+
+# Features to use during clustering
+FEATURES_TO_USE_CLUSTER = ['consecutive_no_transactions_out', 
+                           'Sin_weekday', 'Cos_weekday',
+                           'Sin_hours', 'Cos_hours',
+                          ]
+
+# Station profile rules to determine contamination anomalies
+# based on ROOT_DATA_REF/station_profile.csv
+PROFILE_STATION_RULE = {'very high' : 36, # 6 heures
+                        'hight' : 54,     # 9 heures
+                        'medium' : 66,    # 11 heures
+                        'low' : 144       # 24 heures
+                       }
