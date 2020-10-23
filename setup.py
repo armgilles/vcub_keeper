3fded3cf409d675d81396471244e8561e6ed7786
Gillesa <arm.gilles@gmail.com>
2020-10-23 15:51:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,13 @@ from setuptools import find_packages, setup
 setup(
     name='vcub_keeper',
     packages=find_packages(),
-    version='0.1',
-    description="alerter les stations Vcub qui sont hors service",
+    version='1.0',
+    install_requires=[
+        'pandas==1.1.1',
+        'scikit-learn==0.23.2',
+        'requests==2.24.0'
+    ],
+    description="Alerter les stations Vcub qui sont hors service",
     url="https://github.com/armgilles/vcub_keeper",
     author='GILLES Armand',
     author_email='armand.gilles@sub-data.fr',
