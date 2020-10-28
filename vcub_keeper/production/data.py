1f54e072d3068a4e01b0efe4cf8cffacfa4a1ac4
Gillesa <arm.gilles@gmail.com>
2020-10-28 11:00:31 +0100

    On peut maintenant obtenir l'activité de plusieurs stations en un seul call API. Fix #39

--- vcub_keeper/production/data.py
+++ vcub_keeper/production/data.py
@@ -9,7 +9,7 @@ def get_data_from_api_by_station(station_id, start_date, stop_date):
     
     Parameters
     ----------
-    station_id : Int
+    station_id : Int or List
         Numéro de la station de Vcub
     start_date : str
         Date de début de la Time Serie
@@ -27,6 +27,10 @@ def get_data_from_api_by_station(station_id, start_date, stop_date):
                                                 start_date='2020-10-14',
                                                 stop_date='2020-10-17')
     """
+    
+    if isinstance(station_id, (list, np.ndarray)):
+        station_id = ','.join(map(str, station_id))
+    
     url = \
         "http://data.oslandia.io/bikes/api/bordeaux/timeseries/station/"+ str(station_id) +\
         "?start=" + start_date + "&stop=" + stop_date
@@ -36,7 +40,7 @@ def get_data_from_api_by_station(station_id, start_date, stop_date):
 
 def transform_json_station_data_to_df(station_json):
     """
-    Tranforme la Time Serie d'activité de la station en DataFrame
+    Tranforme la Time Serie d'activité d'une ou plusieurs station en DataFrame
     Effectue plusieurs transformation comme la fonction create/creator.py 
     create_activity_time_series()
         - Structuration
@@ -47,11 +51,11 @@ def transform_json_station_data_to_df(station_json):
     Parameters
     ----------
     station_json : json
-        Time serie au format json de l'activité d'une station
+        Time serie au format json de l'activité d'une station (ou plusieurs)
     Returns
     -------
     station_df_resample : DataFrame
-        Time serie au format DataFrame de l'activité d'une station
+        Time serie au format DataFrame de l'activité d'une ou plusieurs station
         resampler sur 10 min.
         
     Examples
@@ -61,7 +65,15 @@ def transform_json_station_data_to_df(station_json):
     
     """
     
-    station_df = pd.DataFrame(station_json['data'][0])
+    # Si il y a plusieurs stations dans le json
+    if len(station_json['data']) > 1:
+        station_df = pd.DataFrame()
+        for i in range(0, len(station_json['data'])):
+            temp_station_df = pd.DataFrame(station_json['data'][i])
+            station_df = pd.concat([station_df, temp_station_df])
+    # Il y une seule station dans le json
+    else:        
+        station_df = pd.DataFrame(station_json['data'][0])
     
     # Status mapping
     status_dict = {'open' : 1,
