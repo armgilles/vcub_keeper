4cee266209479917591fb015d799789f553705ef
Armand Gilles <arm.gilles@gmail.com>
2020-10-29 17:47:52 +0100

    Try to fix bad env file with heroku (#42)

--- vcub_keeper/config.py
+++ vcub_keeper/config.py
@@ -3,40 +3,48 @@ import numpy as np
 from dotenv import load_dotenv
 load_dotenv()
 
+# Change config env in production
+IS_PROD = False
+
 # Paths
 try:
     ROOT_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 except:
+    # Case of heroku env var
     print("Try to find environnement variable")
     ROOT_DIR = os.environ.get("ROOT_DIR")
-# If package is install with pip, ROOT_DIR is bad
+    IS_PROD = True
+# If package is install with pip & in dev, ROOT_DIR is bad
 if "site-packages" in ROOT_DIR:  # isntall via pip
-    ROOT_DIR = os.environ.get("ROOT_DIR")
+    ROOT_DIR = os.environ.get("ROOT_DIR") # with .env file
 
 ROOT_DATA_RAW = ROOT_DIR + '/data/raw/'
 ROOT_DATA_CLEAN = ROOT_DIR + '/data/clean/'
 ROOT_DATA_REF = ROOT_DIR + '/data/ref/'
 ROOT_MODEL = ROOT_DIR + '/model/'
 
-# ROOT_DATA_RAW
-if not os.path.exists(ROOT_DATA_RAW):
-    os.mkdir(ROOT_DATA_RAW)
-    print('Create '+ROOT_DATA_RAW)
 
-# ROOT_DATA_CLEAN
-if not os.path.exists(ROOT_DATA_CLEAN):
-    os.mkdir(ROOT_DATA_CLEAN)
-    print('Create '+ROOT_DATA_CLEAN)
+# Only in dev
+if IS_PROD is False:
+    # ROOT_DATA_RAW
+    if not os.path.exists(ROOT_DATA_RAW):
+        os.mkdir(ROOT_DATA_RAW)
+        print('Create '+ROOT_DATA_RAW)
 
-# ROOT_DATA_REF
-if not os.path.exists(ROOT_DATA_REF):
-    os.mkdir(ROOT_DATA_REF)
-    print('Create '+ROOT_DATA_REF)
+    # ROOT_DATA_CLEAN
+    if not os.path.exists(ROOT_DATA_CLEAN):
+        os.mkdir(ROOT_DATA_CLEAN)
+        print('Create '+ROOT_DATA_CLEAN)
 
-# ROOT_MODEL
-if not os.path.exists(ROOT_MODEL):
-    os.mkdir(ROOT_MODEL)
-    print('Create '+ROOT_MODEL)
+    # ROOT_DATA_REF
+    if not os.path.exists(ROOT_DATA_REF):
+        os.mkdir(ROOT_DATA_REF)
+        print('Create '+ROOT_DATA_REF)
+
+    # ROOT_MODEL
+    if not os.path.exists(ROOT_MODEL):
+        os.mkdir(ROOT_MODEL)
+        print('Create '+ROOT_MODEL)
 
 SEED = 2020
 
