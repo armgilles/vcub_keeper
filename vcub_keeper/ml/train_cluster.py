8be098e93bd35315210342628f293598075c6ea4
Gillesa <arm.gilles@gmail.com>
2020-11-08 12:03:37 +0100

    Refactoring path directory on reader / cluster / features_factory. Fix #43

--- vcub_keeper/ml/train_cluster.py
+++ vcub_keeper/ml/train_cluster.py
@@ -1,10 +1,11 @@
-from vcub_keeper.config import *
+from vcub_keeper.config import (ROOT_DATA_CLEAN, ROOT_DATA_REF,
+                                ROOT_MODEL,
+                                THRESHOLD_PROFILE_STATION)
 from vcub_keeper.reader.reader import *
-from vcub_keeper.reader.reader_utils import filter_periode
 from vcub_keeper.visualisation import *
 from vcub_keeper.transform.features_factory import *
-from vcub_keeper.ml.cluster import train_cluster_station, predict_anomalies_station
-from vcub_keeper.ml.cluster_utils import load_model, export_model
+from vcub_keeper.ml.cluster import train_cluster_station
+from vcub_keeper.ml.cluster_utils import export_model
 
 
 def run_train_cluster():
@@ -13,12 +14,12 @@ def run_train_cluster():
     """
 
     # Lecture du fichier activité
-    ts_activity = read_time_serie_activity()
+    ts_activity = read_time_serie_activity(path_directory=ROOT_DATA_CLEAN)
     # Some features engi
-    ts_activity = get_consecutive_no_transactions_out(ts_activity) 
+    ts_activity = get_consecutive_no_transactions_out(ts_activity)
 
     # Lecture de profile des stations pour connaitre ceux que l'on clusterise
-    station_profile = read_station_profile()
+    station_profile = read_station_profile(path_directory=ROOT_DATA_REF)
 
     stations_id_to_fit = \
         station_profile[station_profile['mean'] >= THRESHOLD_PROFILE_STATION]['station_id'].unique()
@@ -27,12 +28,12 @@ def run_train_cluster():
     for station_id in stations_id_to_fit:
         # Apprentissage du cluster pour la station
         clf = train_cluster_station(ts_activity, station_id=station_id)
-        
+
         # Export model
-        export_model(clf, station_id=station_id)
+        export_model(clf, station_id=station_id, path_directory=ROOT_MODEL)
 
     print("Fin d'apprentissage des cluster par station ID.")
 
 
 if __name__ == '__main__':
-  run_train_cluster()
+    run_train_cluster()
