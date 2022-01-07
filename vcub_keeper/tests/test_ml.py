7e32167d394e6f96a3388064681cb54a107b7fb4
Gillesa <arm.gilles@gmail.com>
2022-01-07 19:05:48 +0100

    Passed specifiq params to train_cluster_station() fonction to have a specifiq profile_station_activity for CD/CI (#56)

--- vcub_keeper/tests/test_ml.py
+++ vcub_keeper/tests/test_ml.py
@@ -29,6 +29,7 @@ def test_ml_train_on_one_station(data_activity, anomaly):
     station_id=106
     start_date='2018-12-01'
     stop_date='2020-08-28'
+    profile_station_activity='very high' # https://github.com/armgilles/vcub_keeper/issues/56#issuecomment-1007612158
 
     station_json = get_data_from_api_by_station(station_id=station_id, 
                                             start_date=start_date,
@@ -39,7 +40,8 @@ def test_ml_train_on_one_station(data_activity, anomaly):
     # Create feature basé sur l'absence consécutive de prise de vcub sur la station
     station_df = get_consecutive_no_transactions_out(station_df)
 
-    clf = train_cluster_station(station_df, station_id=station_id)
+    clf = train_cluster_station(station_df, station_id=station_id, 
+                                profile_station_activity=profile_station_activity)
 
     #Prediction
     station_df_pred = predict_anomalies_station(data=station_df, clf=clf, station_id=station_id)
