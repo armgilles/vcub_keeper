13a9466ce53d19dd247da84a4c20ea48ed70901c
Armand <arm.gilles@gmail.com>
2024-09-16 14:20:08 +0000

    Ping numpy version to avoid error in CI #82
    Signed-off-by: Armand <arm.gilles@gmail.com>

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     packages=find_packages(),
     version=VERSION,
     install_requires=[
+        'numpy==1.23.3',
         'pandas==1.1.1',
         'scikit-learn==0.23.2',
         'requests==2.28.0',
@@ -15,7 +16,8 @@ setup(
         'matplotlib==3.3.1',
         'seaborn==0.11.0',
         'keplergl==0.2.2',
-        'setuptools==61.3.0'
+        'setuptools==61.3.0',
+        'pytest==6.2.5'
     ],
     description="Alerter les stations Vcub qui sont hors service",
     url="https://github.com/armgilles/vcub_keeper",
