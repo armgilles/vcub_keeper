5f414b5df8f59eb65d5731484a492d6ca1357a04
Armand <arm.gilles@gmail.com>
2024-09-17 08:20:15 +0000

    Format & Ruff check pass #85
    Signed-off-by: Armand <arm.gilles@gmail.com>

--- src/vcub_keeper/transform/features_factory.py
+++ src/vcub_keeper/transform/features_factory.py
@@ -1,5 +1,5 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 
 
 def get_transactions_out(data):
@@ -22,18 +22,16 @@ def get_transactions_out(data):
     activite = get_transactions_out(activite)
     """
 
-    data['available_stands_shift'] = \
-        data.groupby('station_id')['available_stands'].shift(1)
+    data["available_stands_shift"] = data.groupby("station_id")["available_stands"].shift(1)
 
-    data['available_stands_shift'] = data['available_stands_shift'].fillna(data['available_stands'])
+    data["available_stands_shift"] = data["available_stands_shift"].fillna(data["available_stands"])
 
-    data['transactions_out'] = data['available_stands'] - data['available_stands_shift']
+    data["transactions_out"] = data["available_stands"] - data["available_stands_shift"]
 
-    data.loc[data['transactions_out'] < 0,
-             'transactions_out'] = 0
+    data.loc[data["transactions_out"] < 0, "transactions_out"] = 0
 
     # Drop non usefull column
-    data.drop('available_stands_shift', axis=1, inplace=True)
+    data.drop("available_stands_shift", axis=1, inplace=True)
 
     return data
 
@@ -58,18 +56,16 @@ def get_transactions_in(data):
     activite = get_transactions_in(activite)
     """
 
-    data['available_bikes_shift'] = \
-        data.groupby('station_id')['available_bikes'].shift(1)
+    data["available_bikes_shift"] = data.groupby("station_id")["available_bikes"].shift(1)
 
-    data['available_bikes_shift'] = data['available_bikes_shift'].fillna(data['available_bikes'])
+    data["available_bikes_shift"] = data["available_bikes_shift"].fillna(data["available_bikes"])
 
-    data['transactions_in'] = data['available_bikes'] - data['available_bikes_shift']
+    data["transactions_in"] = data["available_bikes"] - data["available_bikes_shift"]
 
-    data.loc[data['transactions_in'] < 0,
-             'transactions_in'] = 0
+    data.loc[data["transactions_in"] < 0, "transactions_in"] = 0
 
     # Drop non usefull column
-    data.drop('available_bikes_shift', axis=1, inplace=True)
+    data.drop("available_bikes_shift", axis=1, inplace=True)
 
     return data
 
@@ -95,15 +91,14 @@ def get_transactions_all(data):
     activite = get_transactions_all(activite)
     """
 
-    data['available_bikes_shift'] = \
-        data.groupby('station_id')['available_bikes'].shift(1)
+    data["available_bikes_shift"] = data.groupby("station_id")["available_bikes"].shift(1)
 
-    data['available_bikes_shift'] = data['available_bikes_shift'].fillna(data['available_bikes'])
+    data["available_bikes_shift"] = data["available_bikes_shift"].fillna(data["available_bikes"])
 
-    data['transactions_all'] = np.abs(data['available_bikes'] - data['available_bikes_shift'])
+    data["transactions_all"] = np.abs(data["available_bikes"] - data["available_bikes_shift"])
 
     # Drop non usefull column
-    data.drop('available_bikes_shift', axis=1, inplace=True)
+    data.drop("available_bikes_shift", axis=1, inplace=True)
 
     return data
 
@@ -138,24 +133,24 @@ def get_consecutive_no_transactions_out(data):
     activite = get_consecutive_no_transactions_out(activite)
     """
 
-    data['have_data'] = 1
-    data.loc[data['available_stands'].isna(),
-             'have_data'] = 0
+    data["have_data"] = 1
+    data.loc[data["available_stands"].isna(), "have_data"] = 0
 
-    data['consecutive_no_transactions_out'] = \
-        data.groupby(['station_id',
-                      (data['available_bikes'] < 3).cumsum(),  # 3 for 2 available_bikes
-                      (data['have_data'] == 0).cumsum(),
-                      (data['status'] == 0).cumsum(),
-                      (data['transactions_out'] > 0).cumsum()]).cumcount()
+    data["consecutive_no_transactions_out"] = data.groupby(
+        [
+            "station_id",
+            (data["available_bikes"] < 3).cumsum(),  # 3 for 2 available_bikes
+            (data["have_data"] == 0).cumsum(),
+            (data["status"] == 0).cumsum(),
+            (data["transactions_out"] > 0).cumsum(),
+        ]
+    ).cumcount()
 
-    data['consecutive_no_transactions_out'] = \
-        data['consecutive_no_transactions_out'].fillna(0)
+    data["consecutive_no_transactions_out"] = data["consecutive_no_transactions_out"].fillna(0)
 
-    data.loc[data['available_stands'].isna(),
-             'consecutive_no_transactions_out'] = 0
+    data.loc[data["available_stands"].isna(), "consecutive_no_transactions_out"] = 0
 
-    data = data.drop('have_data', axis=1)
+    data = data.drop("have_data", axis=1)
 
     return data
 
@@ -163,31 +158,31 @@ def get_consecutive_no_transactions_out(data):
 # https://github.com/armgilles/vcub_keeper/issues/42#issuecomment-718848126
 def get_meteo(data, meteo):
     """
-    AJoute les données météo suivantes :
-        - 'temperature'
-        - 'pressure'
-        - 'humidity' (%)
-       - 'pressure' (mb)
-       - 'precipitation' (mm/h)
-       - 'wind_speed' (m/s)
+     AJoute les données météo suivantes :
+         - 'temperature'
+         - 'pressure'
+         - 'humidity' (%)
+        - 'pressure' (mb)
+        - 'precipitation' (mm/h)
+        - 'wind_speed' (m/s)
 
-    Parameters
-    ----------
-    data : DataFrame
-        Activité des stations Vcub
-    meteo : DataFrame
-        Données météo
+     Parameters
+     ----------
+     data : DataFrame
+         Activité des stations Vcub
+     meteo : DataFrame
+         Données météo
 
-    Returns
-    -------
-    data : DataFrame
-       Ajout des colonnes météo
+     Returns
+     -------
+     data : DataFrame
+        Ajout des colonnes météo
 
-   Examples
-   --------
+    Examples
+    --------
 
-   ts_activity = get_meteo(data=ts_activity, meteo=meteo)
-   """
+    ts_activity = get_meteo(data=ts_activity, meteo=meteo)
+    """
 
     def fast_parse_date_(s):
         """
@@ -198,19 +193,18 @@ def get_meteo(data, meteo):
 
         cf https://github.com/sanand0/benchmarks/tree/master/date-parse
         """
-        dates = {date: date.strftime(format='%Y-%m-%d %H') for date in pd.Series(s.unique())}
+        dates = {date: date.strftime(format="%Y-%m-%d %H") for date in pd.Series(s.unique())}
         return s.apply(lambda v: dates[v])
 
     # Creation des dates au format yyyy_mm avant jointure
-    data['date_year_month_hours'] = fast_parse_date_(data['date'])
-    meteo['date_year_month_hours'] = fast_parse_date_(meteo['date'])
+    data["date_year_month_hours"] = fast_parse_date_(data["date"])
+    meteo["date_year_month_hours"] = fast_parse_date_(meteo["date"])
 
     # Jointure
-    data = data.merge(meteo.drop('date', axis=1),
-                      on='date_year_month_hours', how='left')
+    data = data.merge(meteo.drop("date", axis=1), on="date_year_month_hours", how="left")
 
     # On supprime la colonne 'date_year_month'
-    data = data.drop('date_year_month_hours', axis=1)
+    data = data.drop("date_year_month_hours", axis=1)
 
     return data
 
@@ -238,8 +232,8 @@ def get_encoding_time(data, col_date, max_val):
     data = get_encoding_time(data, 'month', max_val=12)
     """
 
-    data['Sin_'+col_date] = np.sin(2 * np.pi * data[col_date]/max_val)
-    data['Cos_'+col_date] = np.cos(2 * np.pi * data[col_date]/max_val)
+    data["Sin_" + col_date] = np.sin(2 * np.pi * data[col_date] / max_val)
+    data["Cos_" + col_date] = np.cos(2 * np.pi * data[col_date] / max_val)
     return data
 
 
@@ -262,14 +256,14 @@ def process_data_cluster(data):
     data = process_data_cluster(data)
     """
 
-    data['quarter'] = data['date'].dt.quarter
+    data["quarter"] = data["date"].dt.quarter
     # data['month'] = data['date'].dt.month
-    data['weekday'] = data['date'].dt.weekday
-    data['hours'] = data['date'].dt.hour
+    data["weekday"] = data["date"].dt.weekday
+    data["hours"] = data["date"].dt.hour
 
-    data = get_encoding_time(data, 'quarter', max_val=4)
+    data = get_encoding_time(data, "quarter", max_val=4)
     # data = get_encoding_time(data, 'month', max_val=12)
-    data = get_encoding_time(data, 'weekday', max_val=7)
-    data = get_encoding_time(data, 'hours', max_val=24)
+    data = get_encoding_time(data, "weekday", max_val=7)
+    data = get_encoding_time(data, "hours", max_val=24)
 
     return data
