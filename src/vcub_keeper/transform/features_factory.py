88da238fabdbfe01a646cff506b7045025975d78
Armand <arm.gilles@gmail.com>
2024-10-02 08:47:12 +0000

    Try a implementation of Polars migration but Slow I guess # 129
    Signed-off-by: Armand <arm.gilles@gmail.com>

--- src/vcub_keeper/transform/features_factory.py
+++ src/vcub_keeper/transform/features_factory.py
@@ -141,26 +141,37 @@ def get_consecutive_no_transactions_out(data: pl.DataFrame) -> pl.DataFrame:
     activite = get_consecutive_no_transactions_out(activite)
     """
 
-    data = data.to_pandas()
-    data["have_data"] = 1
-    data.loc[data["available_stands"].isna(), "have_data"] = 0
+    # data = data.to_pandas()
+    # data["have_data"] = 1
+    # data.loc[data["available_stands"].isna(), "have_data"] = 0
 
-    data["consecutive_no_transactions_out"] = data.groupby(
-        [
-            "station_id",
-            (data["available_bikes"] < 3).cumsum(),  # 3 for 2 available_bikes
-            (data["have_data"] == 0).cumsum(),
-            (data["status"] == 0).cumsum(),
-            (data["transactions_out"] > 0).cumsum(),
-        ]
-    ).cumcount()
+    # data["consecutive_no_transactions_out"] = data.groupby(
+    #     [
+    #         "station_id",
+    #         (data["available_bikes"] < 3).cumsum(),  # 3 for 2 available_bikes
+    #         (data["have_data"] == 0).cumsum(),
+    #         (data["status"] == 0).cumsum(),
+    #         (data["transactions_out"] > 0).cumsum(),
+    #     ]
+    # ).cumcount()
 
-    data["consecutive_no_transactions_out"] = data["consecutive_no_transactions_out"].fillna(0)
+    # data["consecutive_no_transactions_out"] = data["consecutive_no_transactions_out"].fillna(0)
 
-    data.loc[data["available_stands"].isna(), "consecutive_no_transactions_out"] = 0
+    # data.loc[data["available_stands"].isna(), "consecutive_no_transactions_out"] = 0
 
-    data = data.drop("have_data", axis=1)
-    data = pl.from_pandas(data)
+    # data = data.drop("have_data", axis=1)
+    # data = pl.from_pandas(data)
+
+    data = data.with_columns(
+        pl.int_range(pl.len())
+        .over(
+            pl.col("station_id"),
+            (pl.col("available_stands") <= 3).cum_sum(),
+            (pl.col("status") == 0).cum_sum(),
+            (pl.col("transactions_out") > 0).cum_sum().forward_fill(),  # polars cum_sum ignores nulls
+        )
+        .alias("consecutive_no_transactions_out")
+    )
 
     return data
 
