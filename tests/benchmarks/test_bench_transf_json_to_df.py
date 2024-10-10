3ca099bf30ba290fcf83c500a03576047455e0e1
Armand <arm.gilles@gmail.com>
2024-10-10 17:07:12 +0000

    Adapt code for pipeline bench lazy
    Signed-off-by: Armand <arm.gilles@gmail.com>

--- tests/benchmarks/test_bench_transf_json_to_df.py
+++ tests/benchmarks/test_bench_transf_json_to_df.py
@@ -107,8 +107,16 @@ def test_benchmark_pipepline_transform(json_data=station_json_loaded_simu):
     Benchmark for all transformation steps before ML step.
     """
 
-    station_df = transform_json_api_bdx_station_data_to_df(json_data)
-    station_df = get_consecutive_no_transactions_out(station_df)
+    # station_df = transform_json_api_bdx_station_data_to_df(json_data)
+    # station_df = get_consecutive_no_transactions_out(station_df)
+    # station_df = process_data_cluster(station_df)
+
+    # station_df = station_df.with_columns(
+    #     transform_json_api_bdx_station_data_to_df(json_data), get_consecutive_no_transactions_out()
+    # ).with_columns(process_data_cluster())
+    station_df = transform_json_api_bdx_station_data_to_df(json_data).with_columns(
+        get_consecutive_no_transactions_out()
+    )
     station_df = process_data_cluster(station_df)
 
 
@@ -118,6 +126,7 @@ def test_benchmark_pipepline_transform_big(json_data=station_json_loaded_simu_bi
     Benchmark for all transformation steps before ML step with larger dataset.
     """
 
-    station_df = transform_json_api_bdx_station_data_to_df(json_data)
-    station_df = get_consecutive_no_transactions_out(station_df)
+    station_df = transform_json_api_bdx_station_data_to_df(json_data).with_columns(
+        get_consecutive_no_transactions_out()
+    )
     station_df = process_data_cluster(station_df)
