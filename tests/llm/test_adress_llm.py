8fe75207472a89f962087057b706da93b5e3fe5e
Armand <arm.gilles@gmail.com>
2025-03-10 10:58:11 +0100

    WIP test for find_nearest_stations #208
    Signed-off-by: Armand <arm.gilles@gmail.com>

--- tests/llm/test_adress_llm.py
+++ tests/llm/test_adress_llm.py
@@ -13,7 +13,7 @@ pytestmark = pytest.mark.llm_api  # ou ajout d'un marker sur chaque test - @pyte
 def mock_station_data():
     """Create mock station data for testing"""
     data = {
-        "station_id": [1, 2, 3, 4, 5, 6],
+        "station_id": [1, 2, 3, 4, 5, 103],
         "date": [
             datetime(
                 2025,
@@ -30,27 +30,27 @@ def mock_station_data():
             "St Bruno",
             "Piscine Judaique",
             "St Seurin",
-            "Place Gambetta",
-            "Square Andre Lhote",
+            "Place de la Bourse",
+            "Place du Palais",
         ],
-        "available_stands": [10, 5, 15, 2, 8, 0],
-        "available_bikes": [20, 15, 5, 18, 12, 19],
+        "available_stands": [10, 5, 15, 2, 0, 10],
+        "available_bikes": [20, 15, 5, 18, 40, 22],
         "status": [1, 1, 1, 1, 1, 1],
         "lat": [
             44.83803,
             44.83784,
             44.840813,
             44.84221,
-            44.840714,
-            44.83779,
+            44.840302,
+            44.837799,
         ],
         "lon": [
             -0.58437,
             -0.59028,
             -0.593233,
             -0.58482,
-            -0.581124,
-            -0.58166,
+            44.840302,
+            -0.5703,
         ],
         "anomaly": [1, 1, 1, 1, 1, -1],
         "commune_name": [
@@ -90,6 +90,17 @@ def mock_station_data():
     return df
 
 
+@pytest.fixture
+def agent(mock_station_data):
+    """Create an instance of agent_vcub with mock data"""
+    # Assuming AgentVcub is initialized with a dataframe
+
+    chat_llm = create_chat(model="mistral-small-latest", temperature=0.0)
+    agent_vcub = create_agent(chat=chat_llm, last_info_station=mock_station_data)
+
+    return agent_vcub
+
+
 def test_adresse_geocoding():
     """
     Test de gécodage d'une adresse
@@ -97,3 +108,24 @@ def test_adresse_geocoding():
     lat, lon = get_geocoding("place de la bourse, bordeaux")
     assert abs(lat - 44.8414565) < 0.001
     assert abs(lon - (-0.57037969)) < 0.001
+
+
+def test_message_geocoding(agent):
+    """Test afin de vérifier le geocoding que renvoie l'agent"""
+    user_message = "Quelles sont la latitude et longitude de 12 Rue des Faussets Bordeaux uniquement ?"
+    response = agent.invoke({"input": user_message})
+    # La latitude est 44.8404215 et la longitude est -0.5704848
+
+    assert "44.840" in response["output"]  # lat : 44.8404215
+
+
+def test_message_station_near_adress(agent):
+    """Test à partir d'une adresse de trouver les 2 stations les plus proches"""
+    # user_message = "Quelles sont les 2 stations les plus proche de 12 Rue des Faussets Bordeaux ?"
+    user_message = (
+        "Quelles sont les 2 stations les plus proche de La latitude est 44.8404215 et la longitude est -0.5704848 ?"
+    )
+    response = agent.invoke({"input": user_message})
+
+    assert "Place du Palais".lower() in response["output"].lower()
+    assert "Place de la Bourse".lower() in response["output"].lower()
