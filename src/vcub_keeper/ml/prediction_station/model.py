5550640c05881eba05123c62d0746ef5825100a2
Armand <arm.gilles@gmail.com>
2025-03-11 16:29:45 +0100

    Add a fonction to create features to use in model depend target #210
    Signed-off-by: Armand <arm.gilles@gmail.com>

--- src/vcub_keeper/ml/prediction_station/model.py
+++ src/vcub_keeper/ml/prediction_station/model.py
@@ -37,3 +37,49 @@ def train_model_for_station(
     model.fit(x_train, y_train)
 
     return model
+
+
+def get_feature_to_use_for_model(
+    target_col: str,
+) -> list[str]:
+    """
+    Get the features to use for the model
+
+    Parameters
+    ----------
+    station_to_pred : pl.DataFrame
+        Data of the station to predict
+
+    Returns
+    -------
+    feat_to_use : list[str]
+        List of features to use for the model
+
+    Example
+    -------
+    feat_to_use = get_feature_to_use_for_model(target_col="available_stands")
+    """
+
+    feat_to_use = [
+        "Sin_weekday",
+        "Cos_weekday",
+        "Sin_hours",
+        "Cos_hours",
+        "Sin_minutes",
+        "Cos_minutes",
+        # lag
+        f"{target_col}_lag_1",
+        f"{target_col}_lag_2",
+        f"{target_col}_lag_3",
+        # rolling min / max
+        f"{target_col}_rolling_max_6",
+        f"{target_col}_rolling_max_12",
+        f"{target_col}_rolling_max_1d",
+        f"{target_col}_rolling_max_7d",
+        f"{target_col}_rolling_min_6",
+        f"{target_col}_rolling_min_12",
+        f"{target_col}_rolling_min_1d",
+        f"{target_col}_rolling_min_7d",
+    ]
+
+    return feat_to_use
